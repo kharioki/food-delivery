aef651ba4c085364305f1494eafdea3e0385ec5a
kharioki <antolisah@gmail.com>
2020-06-06 23:16:06 +0300

    food model and displaying food items

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery/core/consts.dart';
 import 'package:food_delivery/core/flutter_icons.dart';
+import 'package:food_delivery/models/food_model.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,6 +9,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<FoodModel> foodList = FoodModel.list;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +19,70 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           children: <Widget>[
             _customAppBar(),
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  Text('Build Pageview'),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 40),
+                    child: Text(
+                      'Popular',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 32,
+                      ),
+                    ),
+                  ),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: foodList.length,
+                    itemBuilder: (context, index) => Container(
+                      child: Row(
+                        children: <Widget>[
+                          Image(
+                            width: 100,
+                            image: AssetImage(
+                                'assets/images/${foodList[index].imgPath}'),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                '${foodList[index].name}',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Row(
+                                children: <Widget>[
+                                  Text(
+                                    '\$${foodList[index].price.toInt()}',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      color: AppColors.redColor,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(width: 16),
+                                  Text(
+                                    '(${foodList[index].weight.toInt()}gm Weight)',
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
@@ -65,7 +132,10 @@ class _HomePageState extends State<HomePage> {
                       ),
                     ),
                   ),
-                  Icon(FlutterIcons.search),
+                  Icon(
+                    FlutterIcons.search,
+                    size: 16,
+                  ),
                 ],
               ),
             ),
@@ -80,7 +150,10 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
             child: Center(
-              child: Icon(FlutterIcons.shop),
+              child: Icon(
+                FlutterIcons.shop,
+                size: 16,
+              ),
             ),
           ),
         ],
