19e4fda8dd03b011f5605b0f71787dc769806dc2
kharioki <antolisah@gmail.com>
2020-06-07 04:53:48 +0300

    item details on detail page

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -5,7 +5,7 @@ import 'package:food_delivery/models/food_model.dart';
 
 class DetailPage extends StatefulWidget {
   final FoodModel data;
-  const DetailPage(FoodModel foodList, {this.data});
+  const DetailPage(this.data);
 
   @override
   _DetailPageState createState() => _DetailPageState();
@@ -16,23 +16,88 @@ class _DetailPageState extends State<DetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.greenColor,
-      body: Column(children: <Widget>[
-        _customAppBar(),
-              ],),
-            );
-          }
-        
-          Widget _customAppBar() {
-            return Row(
-              children: <Widget>[
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(12),),
-                  ),
-                  child: Icon(FlutterIcons.left,),
+      body: Column(
+        children: <Widget>[
+          SizedBox(height: 25),
+          _customAppBar(),
+          Text('Food details:'),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40),
+                  topRight: Radius.circular(40),
                 ),
-              ],
-            )
-          }
+              ),
+              child: Column(
+                children: <Widget>[
+                  Text('${widget.data.name}'),
+                  Row(
+                    children: <Widget>[
+                      Text('\$${widget.data.price.toInt()}'),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      _buildInfo('Weight', '${widget.data.weight.toInt()}gm'),
+                      _buildInfo(
+                          'Calories', '${widget.data.calory.toInt()}ccal'),
+                      _buildInfo('Protein', '${widget.data.protein.toInt()}gm'),
+                    ],
+                  ),
+                  Text('Items'),
+                  Text('${widget.data.item}'),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildInfo(String title, String val) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[Text(title), Text('${val}')],
+    );
+  }
+
+  Widget _customAppBar() {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(
+                Radius.circular(12),
+              ),
+            ),
+            child: Icon(
+              FlutterIcons.left,
+              size: 16,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(
+                Radius.circular(12),
+              ),
+            ),
+            child: Icon(
+              FlutterIcons.shop,
+              size: 16,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
